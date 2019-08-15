0a5794f8a303a7c1ddeadc33b2617029ef113663
Catherine <catherinej@vt.edu>
2019-08-14 20:38:34 -0400

    'Made it work with geoclaw inputs'

--- b4step2.f90
+++ b4step2.f90
@@ -51,128 +51,151 @@ subroutine b4step2(mbc,mx,my,meqn,q,xlower,ylower,dx,dy,t,dt,maux,aux)
         q(2:3,i,j) = 0.d0
     end forall
 	
-   if ((xlower > -85.0) .and. (xlower < -80.0) .and. &
-	(ylower < 26) .and. (ylower > 24)) then
-		
-			print *, 'Gonna call barrier breach!'
-			call barrier_breach()
-		
-	end if
-	
+
     if (aux_finalized < 2) then
+        print *, 'RESETTING AUX'
         ! topo arrays might have been updated by dtopo more recently than
         ! aux arrays were set unless at least 1 step taken on all levels
         aux(1,:,:) = NEEDS_TO_BE_SET ! new system checks this val before setting
         call setaux(mbc,mx,my,xlower,ylower,dx,dy,maux,aux)
     endif
 
+    ! !!!!!Change xlower and ylower compare values to varialbes read from file 
+   if ((xlower > -88.0) .and. (xlower < -86.0) .and. &
+        (ylower < 25.7) .and. (ylower > 25.5)) then
+       
+           print *, 'Gonna call barrier breach!'
+           call barrier_breach(maux,mbc,mx,my,xlower,ylower,dx,dy,t,dt,aux)
+       
+   end if
+   
     ! Set wind and pressure aux variables for this grid
     call set_storm_fields(maux,mbc,mx,my,xlower,ylower,dx,dy,t,aux)
 
 end subroutine b4step2
-subroutine barrier_breach()
+
+subroutine barrier_breach(maux,mbc,mx,my,xlower,ylower,dx,dy,t,dt,aux)
 implicit none
 
 ! Allocate variables
-integer, parameter :: dp = selected_real_kind(15, 307)
-real, allocatable, dimension(:) :: bathy, row
-integer :: i, j, mx, my , t1, t2, rowidx, colidx
-real :: xll, yll, cellsize, nodata_value, amp_max, dx, sigma, h0, x1, x2, col, stepx, stepy
+! Subroutine variables
+integer, intent(inout) :: mbc,mx,my,maux
+real(kind=8), intent(inout) :: xlower, ylower, dx, dy, t, dt
+real(kind=8), intent(inout) :: aux(maux,1-mbc:mx+mbc,1-mbc:my+mbc)
+
+! Local variables
+! integer, parameter :: dp = selected_real_kind(15, 307)
+real, allocatable, dimension(:) ::  row
+integer :: i, j,  t1, t2, rowidx, colidx
+real ::  amp_max, sigma, h0, x1, x2, col, stepx, stepy, dtime, amp
 real :: time(10) != (/(i, i=0,9, 1)/)
-real, dimension(10) :: amp
+!real, dimension(10) :: amp
 integer, dimension(10) :: zero_array
 integer, dimension(10) :: one_array
 real, dimension(600) :: y 
 real,allocatable, dimension(:,:) :: X_mesh, Y_mesh, breach
 real, dimension(500) :: x 
 CHARACTER(*), PARAMETER :: fileplace = "/home/cat/claw/clawpack-v5.5.0/geoclaw/scratch/"
+
+! do j=1-mbc,my+mbc
+    !     y = ylower + (j-0.5d0) * dy
+    !     do i=1-mbc,mx+mbc
+    !         x = xlower + (i-0.5d0) * dx
+    !         ! Location logic
+    !         aux(1, i, j) = something
+    !     end do
+    ! end do
+
 ! Determine number of increments for X_mesh and Y_mesh
-stepx = (100.0 + 100.0)/ 499.0
-stepy = (60e3 / 599)
+! stepx = (100.0 + 100.0)/ 499.0
+! stepy = (60e3 / 599)
 
 ! fill x and y like np.linspace
-x = (/((i-1)*stepx -100.0, i=1,500)/)
-y = (/((i-1)* stepy + 0, i=1, 600)/)
+! x = (/((i-1)*stepx -100.0, i=1,500)/)
+! y = (/((i-1)* stepy + 0, i=1, 600)/)
 
-! ! Open bathy file and read header to get size
-! open(10, file=fileplace//'test.tt3', status='old')
-	! read(10,*) mx
-	! read(10,*) my
-	! read(10,*) xll
-	! read(10,*) yll
-	! read(10,*) cellsize
-	! read(10,*) nodata_value
-! close(10)
+do j=1-mbc,my+mbc
+        y = ylower + (j-0.5d0) * dy
+        do i=1-mbc,mx+mbc
+            x = xlower + (i-0.5d0) * dx
+            print *, size(x), size(y)
+        end do
+    end do
 
-! Declare Amplitude array based on time steps
-t1 = 3.0
+t1 = -200000.0
 t2 = 6.0
 amp_max = 5.0
-dx = 10.0/9.0
-time(1:10) = [(0 + ((i-1)*dx),i=1, 10)]
+dtime = 10.0/9.0 !!!!! what is this doing!
+time(1:10) = [(0 + ((i-1)*dtime),i=1, 10)]
 
 zero_array = 0
 one_array = 1
 
-do i = 1, 10
-	if (time(i) < t1) then
-		amp(i) = zero_array(i)
-	elseif (t1 <= time(i) .and. time(i) < t2) then
-		amp(i) =  (amp_max / (t2 - t1) * (time(i) - t1))
-	elseif (t2 <= time(i)) then
-		amp(i) = one_array(i) * amp_max
-end if
-	
-end do
-! Open and read bathymetry into memory
-! Skip header and declare bathy matrix
-! allocate(bathy(1:mx*my))
-! open(11, file=fileplace//'test.tt3', status='old')
-! do i=1,6
-    ! read(11, *)
-! end do
-! ! Read bathy data into array
-! do j=1, my !*nrows
-   ! read(11, *) (bathy((j-1)* mx + i),i=1,mx)
-! end do
-! close(11)
+!do i = 1, 10
+!	if (time(i) < t1) then
+!		amp(i) = zero_array(i)
+!	elseif (t1 <= time(i) .and. time(i) < t2) then
+!		amp(i) =  (amp_max / (t2 - t1) * (time(i) - t1))
+!	elseif (t2 <= time(i)) then
+!		amp(i) = one_array(i) * amp_max
+!end if
+!
+!end do
 
 ! fill x and y matrices a la np.meshgrid
 allocate(X_mesh(size(y), size(x)))
 allocate(Y_mesh(size(y), size(x)))
 do i=1, size(y)
-	do j=1, size(x)
-		X_mesh(i,j) = x(j)
-		Y_mesh(i,j) = y(i)
-	end do
+    do j=1, size(x)
+        X_mesh(i,j) = x(j)
+        Y_mesh(i,j) = y(i)
+    end do
 end do
 ! Add breach parameters
-x1 = -25.0
-x2 = 25.0
+!!!!!! change these to imported from b4step2 subroutine
+x1 = -86.0
+x2 = -88.0
 sigma = 10.0
 h0 = 10.0
 allocate(breach(size(X_mesh, 1), size(X_mesh, 2)))
 ! Initialize breach to match initial height
-do i= 1, size(X_mesh,2)
-	breach(:,i) = h0
-	do j = 1, size(X_mesh,1)
-		breach(j,:) = h0
-	end do
-end do
+!do i= 1, size(X_mesh,2)
+!	breach(:,i) = h0
+!	do j = 1, size(X_mesh,1)
+!		breach(j,:) = h0
+!	end do
+!end do
+breach(:,:) = aux(1, :, :)
 ! Breach the barrier island depending on time
-do i = 1, size(amp)
+if (t<t1) then
+    amp =0.0
+elseif (t>t1) then
+    amp=10.0
+end if
+
+!do i = 1, size(amp)
 	do rowidx = 1, size(X_mesh, 1)
 		row = X_mesh(rowidx,:)
 			do colidx = 1, size(X_mesh, 2)
 				col = X_mesh(rowidx, colidx)
 				if (col >= x1 .and. col <= x2) then
-					breach(rowidx, colidx) = breach(rowidx, colidx) & 
-					- (amp(i) * exp(-X_mesh(rowidx, colidx)**2/(sigma**2)) &
+					breach(rowidx, colidx) = breach(rowidx, colidx) &
+					- (amp * exp(-X_mesh(rowidx, colidx)**2/(sigma**2)) &
 					* 0.1 * Y_mesh(rowidx, colidx))
 				end if
 			end do
 	end do
-end do
+!end do
 
+aux(1,:,:) = breach(:,:)
+print *, maxval(breach)
+! do j=1-mbc,my+mbc
+    !     y = ylower + (j-0.5d0) * dy
+    !     do i=1-mbc,mx+mbc
+    !         x = xlower + (i-0.5d0) * dx
+    !         ! Location logic
+    !         aux(1, i, j) = something
+    !     end do
+    ! end do
 
 end subroutine barrier_breach
